8c766726ce15f68b6a359425c71a593fb9cbec38
Herton Julius Pratama <hertonjulius@gmail.com>
2023-12-09 01:03:42 +0700

    Revert "Merge branch 'richard' of https://github.com/HertonJP/GarenaGameJam into richard"
    This reverts commit 377a21041c256b5c270f61f8ebc89128592eb144, reversing
    changes made to f33ca57805ba705b4dc03458a88568bb59f7530f.

---
+++ Assets/Scripts/TalentBase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TalentBase : MonoBehaviour
+{
+    public virtual void ImplementTalent()
+    {
+
+    }
+    public virtual void ImplementTalent(Sword sword)
+    {
+
+    }
+}
