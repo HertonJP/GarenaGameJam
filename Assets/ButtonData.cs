2dde670536bef42d929fbda7c59e0cd16c3f4010
Herton Julius Pratama <hertonjulius@gmail.com>
2023-12-09 19:08:32 +0700

    Revert "Merge branch 'herton2' of https://github.com/HertonJP/GarenaGameJam into herton2"
    This reverts commit da259d52784e21e17ce229ddad5b03f2c75c75fe, reversing
    changes made to a989d34ac6ac8dbcf80ae60894720d811f69ec9f.

--- Assets/ButtonData.cs
+++ Assets/ButtonData.cs
@@ -1,33 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class ButtonData : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+public class ButtonData : MonoBehaviour
 {
     public string skillName;
-    public Image buttonImage;
-    public Sprite hoverSprite;
     // Start is called before the first frame update
     void Start()
     {
-        buttonImage = GetComponent<Image>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        buttonImage.color = new Color(buttonImage.color.r, buttonImage.color.g, buttonImage.color.b, 255);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        buttonImage.color = new Color(buttonImage.color.r, buttonImage.color.g, buttonImage.color.b, 0);
+        
     }
 }
